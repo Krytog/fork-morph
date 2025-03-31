67fb0d6d735fb62150ded78437d40d93c17a6f73
Krytog <danya2003.db@gmail.com>
2025-03-31 15:58:10 +0300

    Add 3rdparty/aws

--- src/DataProviders.cpp
+++ src/DataProviders.cpp
@@ -1,6 +1,6 @@
 #include "DataProviders.h"
 
-// #include <S3.h>
+#include <S3.h>
 
 #include <utility>
 #include <cstring>
@@ -21,7 +21,7 @@ std::string FileChunkProvider::GetChunk(size_t from, size_t to) {
     return file_reader_.ReadChunk(offset, size);
 }
 
-/*
+
 
 S3ChunkProvider::S3ChunkProvider(const std::string& url) : url_(url) {
 }
@@ -31,8 +31,6 @@ std::string S3ChunkProvider::GetChunk(size_t from, size_t to) {
     const auto chunk = readPartialS3Object(url_, offset, size);
     std::string output;
     output.resize(chunk.size());
-    std::memcpy(chunk.data(), output.data(), chunk.size());
+    std::memcpy(output.data(),chunk.data(), chunk.size());
     return output;
 }
-
-*/
